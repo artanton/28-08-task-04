3a819cef1b9b7176f64349e143fe8aab3a0423b4
artanton <kabletrade@gmail.com>
2024-08-28 17:02:16 +0300

    initial

--- src/index.tsx
+++ src/index.tsx
@@ -11,7 +11,7 @@ import { BrowserRouter } from 'react-router-dom';
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename="/12-08-task-ts-no3">
+      <BrowserRouter basename="/28-08-task-04">
         <ThemeProvider theme={theme}>
           <App />
         </ThemeProvider>
